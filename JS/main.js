88831c4e14c240f723b6c56f30641468f52ab7b5
Ranjeet-Rawat <ranjeetsingh01266@gmail.com>
2022-08-24 23:46:50 +0530

    swiper js done

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,28 @@ let bell = document.querySelector(".notification");
 document.querySelector('#bell-icon').onclick= () =>{
     bell.classList.toggle('active');
 }
+
+var swiper = new Swiper(".trending-content", {
+    loop:true,
+    grabCursor:true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
